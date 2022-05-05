2663e38e4d15ac833bbe660db5b20078b98399e2
angelDariaux <angeldariaux@gmail.com>
2022-05-04 20:03:49 -0600

    creado un forEach de lo que nos devuelve la API cuando hacemos el fetch, ahora vamos a hacer los appendChild

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
-// https://api.themoviedb.org/3/
+// https://api.themoviedb.org/3/      // * URL base para la API
+// https://image.tmdb.org/t/p/w300/    // * URL base para las imagenes
+
 
 const getTrendingMoviesPreview = async () => { 
   const res = await fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`);
   const data = await res.json();
 
   const movies = data.results;
+  movies.forEach(movie => { 
+    const movieContainer = document.createElement('div');
+    movieContainer.classList.add('movie-container');
+
+    const movieImg = document.createElement('img');
+    movieImg.classList.add('movie-img');
+    movieImg.setAttribute('alt', movie.title);
+    movieImg.setAttribute('src', `https://image.tmdb.org/t/p/w300${movie.poster_path}`)
+  })
 }
 
 getTrendingMoviesPreview();
