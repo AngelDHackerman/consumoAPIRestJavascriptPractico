69bafbd4e4e9ef8bc15eec65bca73643c4c1d891
angelDariaux <angeldariaux@gmail.com>
2022-05-04 20:13:58 -0600

    agregados los appendChild ya tenemos la API consumida y nos muestra en las tendencias en peliculas!

--- src/main.js
+++ src/main.js
@@ -8,14 +8,21 @@ const getTrendingMoviesPreview = async () => {
 
   const movies = data.results;
   movies.forEach(movie => { 
+    const trendingPreviewMoviesContainer = document.querySelector(
+      '#trendingPreview .trendingPreview-movieList'
+    )
+
     const movieContainer = document.createElement('div');
     movieContainer.classList.add('movie-container');
 
     const movieImg = document.createElement('img');
     movieImg.classList.add('movie-img');
     movieImg.setAttribute('alt', movie.title);
-    movieImg.setAttribute('src', `https://image.tmdb.org/t/p/w300${movie.poster_path}`)
-  })
+    movieImg.setAttribute('src', `https://image.tmdb.org/t/p/w300${movie.poster_path}`);
+    
+    movieContainer.appendChild(movieImg);
+    trendingPreviewMoviesContainer.appendChild(movieContainer);
+  });
 }
 
 getTrendingMoviesPreview();
