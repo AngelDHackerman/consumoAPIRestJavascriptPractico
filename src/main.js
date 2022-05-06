0be81e85bf800aee489024d459acd07e59798e31
angelDariaux <angeldariaux@gmail.com>
2022-05-05 19:26:49 -0600

    usando git stash y usando axios para la funcion de trending movies preview

--- src/main.js
+++ src/main.js
@@ -18,10 +18,9 @@ const api = axios.create({
       // ! Imprimiento el poster de las peliculas
 
 const getTrendingMoviesPreview = async () => { 
-  const res = await fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`);
-  const data = await res.json();
-
+  const { data } = await api('trending/movie/day');
   const movies = data.results;
+
   movies.forEach(movie => { 
     const trendingPreviewMoviesContainer = document.querySelector(
       '#trendingPreview .trendingPreview-movieList'
@@ -43,10 +42,9 @@ const getTrendingMoviesPreview = async () => {
       // ! Mostrando el preview de las categorias
 
 const getCategoriesPreview = async () => {
-  const res = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`);
-  const data = await res.json();
+  const { data } = await api('genre/movie/list');
+  const categories = data.genres;
 
-  const categories = data.genres; 
   categories.forEach(category => {
     const previewCategoriesContainer = document.querySelector(
       '#categoriesPreview .categoriesPreview-list'
