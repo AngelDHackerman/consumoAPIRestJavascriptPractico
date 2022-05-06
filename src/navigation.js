e404b316be785703d2b46d457717fba7bbc56bec
angelDariaux <angeldariaux@gmail.com>
2022-05-05 20:00:36 -0600

    agregando el archivo navigation.js para ver los hash donde navegamos

---
+++ src/navigation.js
@@ -0,0 +1,40 @@
+      // ! Funcion navigator para ver en que hash stamos
+      
+const navigator = () => { 
+  console.log( { location }); 
+  
+  if (location.hash.startsWith('#trends')) { 
+    trendsPage()
+  } else if (location.hash.startsWith('#search=')) { 
+    searchPage();
+  } else if (location.hash.startsWith('#movie=')) { 
+    moviePage();
+  } else if (location.hash.startsWith('#category=')) { 
+    categoriesPage();
+  } else { 
+    homePage();
+  }
+}
+
+const trendsPage = () => { 
+  console.log('Trends!!!')
+}
+
+const searchPage = () => { 
+  console.log('Search!');
+}
+
+const moviePage = () => { 
+  console.log('Movie!!!');
+}
+
+const categoriesPage = () => { 
+  console.log('Categories!!!');
+}
+
+const homePage = () => { 
+  console.log('Home!');
+}
+
+window.addEventListener('DOMContentLoaded', navigator, false);
+window.addEventListener('hashchange', navigator, false); 
