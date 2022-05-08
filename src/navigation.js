c1b58665b5c6e9ce34aada6a449142e3482b4463
angelDariaux <angeldariaux@gmail.com>
2022-05-08 14:20:07 -0600

    agregando el codigo de nodes a las otras paginas de js a las secciones de movieDetailPage

--- src/navigation.js
+++ src/navigation.js
@@ -8,7 +8,7 @@ const navigator = () => {
   } else if (location.hash.startsWith('#search=')) { 
     searchPage();
   } else if (location.hash.startsWith('#movie=')) { 
-    moviePage();
+    movieDetailsPage();
   } else if (location.hash.startsWith('#category=')) { 
     categoriesPage();
   } else { 
@@ -16,28 +16,94 @@ const navigator = () => {
   }
 }
 
-const trendsPage = () => { 
-  console.log('Trends!!!')
-}
-
-const searchPage = () => { 
-  console.log('Search!');
-}
-
-const moviePage = () => { 
-  console.log('Movie!!!');
-}
-
-const categoriesPage = () => { 
-  console.log('Categories!!!');
-}
-
 const homePage = () => { 
   console.log('Home!');
+
+  // ? Agregando el codigo que viene de nodes.js 
+
+  headerSection.classList.remove('header-container--long');
+  headerSection.style.background = '';
+  arrowBtn.classList.add('inactive'); 
+  arrowBtn.classList.add('header-arrow--white');
+  headerTitle.classList.remove('inactive');
+  headerCategoryTitle.classList.add('inactive');
+  searchForm.classList.remove('inactive');
+
+  trendingPreviewSection.classList.remove('inactive');
+  categoriesPreviewSection.classList.remove('inactive');
+  genericSection.classList.add('inactive');
+  movieDetailSection.classList.add('inactive');
+
+  // ? Llamando las funciones de la hoja main.js
 
   getTrendingMoviesPreview();
   getCategoriesPreview();
 }
+
+const categoriesPage = () => { 
+  console.log('Categories!!!');
+
+  // ? Agregando el codigo que viene de nodes.js 
+
+  headerSection.classList.remove('header-container--long');
+  headerSection.style.background = '';
+  arrowBtn.classList.remove('inactive'); 
+  arrowBtn.classList.remove('header-arrow--white');
+  headerTitle.classList.add('inactive');
+  headerCategoryTitle.classList.remove('inactive');
+  searchForm.classList.add('inactive');
+
+  trendingPreviewSection.classList.add('inactive');
+  categoriesPreviewSection.classList.add('inactive');
+  genericSection.classList.remove('inactive');
+  movieDetailSection.classList.add('inactive');
+}
+
+const movieDetailsPage = () => { 
+  console.log('Movie!!!');
+
+  // ? Agregando el codigo que viene de nodes.js 
+
+  headerSection.classList.add('header-container--long');
+  // headerSection.style.background = '';
+  arrowBtn.classList.remove('inactive'); 
+  arrowBtn.classList.add('header-arrow--white');
+  headerTitle.classList.add('inactive');
+  headerCategoryTitle.classList.add('inactive');
+  searchForm.classList.add('inactive');
+
+  trendingPreviewSection.classList.add('inactive');
+  categoriesPreviewSection.classList.add('inactive');
+  genericSection.classList.add('inactive');
+  movieDetailSection.classList.remove('inactive');
+}
+
+const searchPage = () => { 
+  console.log('Search!');
+
+  // ? Agregando el codigo que viene de nodes.js 
+
+  headerSection.classList.remove('header-container--long');
+  headerSection.style.background = '';
+  arrowBtn.classList.remove('inactive'); 
+  arrowBtn.classList.remove('header-arrow--white');
+  headerTitle.classList.add('inactive');
+  headerCategoryTitle.classList.remove('inactive');
+  searchForm.classList.add('inactive');
+
+  trendingPreviewSection.classList.add('inactive');
+  categoriesPreviewSection.classList.add('inactive');
+  genericSection.classList.remove('inactive');
+  movieDetailSection.classList.add('inactive');
+}
+
+const trendsPage = () => { 
+  console.log('Trends!!!')
+}
+
+
+
+
 
 window.addEventListener('DOMContentLoaded', navigator, false);
 window.addEventListener('hashchange', navigator, false); 
